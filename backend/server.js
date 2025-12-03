5f2a2f989b38613d9655ffac508ad8c0f86f470a
Neeraj Singh <neerajsingh@Neerajs-MacBook-Pro-2.local>
2025-12-03 23:46:16 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
 }));
 app.use(express.json());
